c9d5979c96469375469f0e3b8e27831738bf3637
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-04-01 14:16:14 +0200

    Create DealershipApp.java

---
+++ src/DealershipApp.java
@@ -0,0 +1,28 @@
+public class DealershipApp {
+    public static void main(String[] args) {
+
+        // 🔧 Task 2: Create car objects
+        // TODO: Create at least 3 Car objects with different data
+
+        // 🔧 Task 2: Display car info
+        // TODO: Call displayInfo() on each car
+
+        // 🔧 Task 3: Static variable behavior
+        // TODO: Print dealer name, change it, and show that all cars reflect the change
+
+        // 🔧 Task 4: Total cars created
+        // TODO: Print total number of cars created using static method
+
+        // 🔧 Task 6: Update price
+        // TODO: Call setPrice() and print updated car info
+
+        // 🔧 Task 7: Apply discount
+        // TODO: Apply discount to one or more cars
+
+        // 🔧 Task 8: Depreciate price
+        // TODO: Call depreciate(currentYear) and print results
+
+        // 🔧 Task 9: DealerStats static methods
+        // TODO: Call DealerStats.printWelcomeMessage() and other utility methods
+    }
+}
